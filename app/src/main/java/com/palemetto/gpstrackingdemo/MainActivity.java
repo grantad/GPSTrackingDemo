036aec55141b2488d446e5e8897259e9944397ed
anthonygrant <grant.anthony@gmail.com>
2022-07-11 19:12:08 -0400

    removed depricated class locationRequest

--- app/src/main/java/com/palemetto/gpstrackingdemo/MainActivity.java
+++ app/src/main/java/com/palemetto/gpstrackingdemo/MainActivity.java
@@ -1,15 +1,18 @@
 package com.palemetto.gpstrackingdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
@@ -21,6 +24,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 public class MainActivity extends AppCompatActivity {
     private static final int DEFAULT_UPDATE_INTERVAL = 5;
     public static final int FAST_UPDATE_INTERVAL = 30;
+    private static final int Permissions_Fine_Locations = 99;
 
     TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
 
@@ -56,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         // set all properties of LocationRequest
 
-        locationRequest = new LocationRequest();
+        locationRequest = LocationRequest.create();
+
 
 
         // how often does teh default location check occur?
@@ -65,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         // how often does the location check occur when set to the most frequest update?
         locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
 
-        locationRequest.setPriority(locationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setPriority( Priority.PRIORITY_BALANCED_POWER_ACCURACY);
 
         sw_gps.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +87,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        updateGPS();
+
     }  // end of onCreate method
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case Permissions_Fine_Locations:;
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                updateGPS();
+            }
+            else {
+                Toast.makeText(this, "This app requires permission to be granted in order to work properly... You SUCK!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            break;
+        }
+    }
 
     private void updateGPS() {
         // get permissions from the user to track GPS
@@ -97,12 +121,41 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(Location location) {
                     // we got permissions. Put the values of location.  XXX into the UI components.
+                    updateUIValue(location);
                 }
-            })
+            });
         }
         else {
             // permissions not granted yet.
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, Permissions_Fine_Locations );
+            }
         }
+    }
+
+    private void updateUIValue(Location location) {
+
+        // update all of the text view objects with a new location.
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+
+        // Not all phones have altitude and speed test for it
+        if (location.hasAltitude()) {
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }
+        else {
+            tv_altitude.setText(("Not available"));
+        }
+
+        if (location.hasSpeed()) {
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }
+        else {
+            tv_speed.setText(("Not available"));
+        }
+
     }
 
 
